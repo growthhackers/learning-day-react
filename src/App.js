aac173ce33f0f224034d5393680afdc55c0d90e0
Ronny Cruz <ronnyfvcruz@gmail.com>
2020-02-26 18:24:34 -0300

    update and delete async

--- src/App.js
+++ src/App.js
@@ -1,155 +1,188 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import logo from "./logo.svg";
 import { Row, Col, Layout, Spin } from "antd";
 import FormCar from "./components/FormCar";
 import Cars from "./components/FormCar/Cars";
-import './App.css';
-import axios from 'axios';
+import "./App.css";
+import axios from "axios";
 
-const apiURL = 'https://crudcrud.com/api/2ee8a8e55ba74c49a066016b024d3b23/cars';
+const apiURL = "https://crudcrud.com/api/67e7d97d03bc4fe5bcdbd946943c6414/cars";
 
-const axiosGet = (url) => {
+const axiosGet = url => {
   return new Promise((resolve, reject) => {
     axios
       .get(url)
       .then(
-        (response) => {
-          resolve(response.data)
+        response => {
+          resolve(response.data);
         },
-        (error) => {
-          reject(error)
+        error => {
+          reject(error);
         }
       )
-      .catch((error) => {
-        reject(error)
-      })
-  })
-}
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
 
 const axiosPost = (url, payload) => {
   return new Promise((resolve, reject) => {
     axios
       .post(url, payload)
       .then(
-        (response) => {
-          resolve(response.data)
+        response => {
+          resolve(response.data);
         },
-        (error) => {
-          reject(error)
+        error => {
+          reject(error);
         }
       )
-      .catch((error) => {
-        reject(error)
-      })
-  })
-}
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
+
+const axiosPut = (url, payload) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .put(url, payload)
+      .then(
+        response => {
+          resolve(response.data);
+        },
+        error => {
+          reject(error);
+        }
+      )
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
 
 const axiosDelete = (url, id) => {
   return new Promise((resolve, reject) => {
     axios
       .delete(`${url}/${id}/`)
       .then(
-        (response) => {
-          resolve(response.data)
+        response => {
+          resolve(response.data);
         },
-        (error) => {
-          reject(error)
+        error => {
+          reject(error);
         }
       )
-      .catch((error) => {
-        reject(error)
-      })
-  })
-}
-
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      cars: null,
+      cars: null
     };
-  };
-
-  componentDidMount() {
-    axiosGet(apiURL)
-      .then((cars) => {
-        this.setState({cars});
-      },
-      (error) => {
-        console.log('Error message: ' + error);
-      });
   }
 
-  addCar = (carName) => {
-    const {cars} = this.state;
+  componentDidMount() {
+    axiosGet(apiURL).then(
+      cars => {
+        this.setState({ cars });
+      },
+      error => {
+        console.log("Error message: " + error);
+      }
+    );
+  }
+
+  addCar = carName => {
+    const { cars } = this.state;
 
     return new Promise((resolve, reject) => {
       axiosPost(apiURL, { name: carName }).then(
-        (newCar) => {
+        newCar => {
           this.setState({
-            cars: [...cars, newCar],
+            cars: [...cars, newCar]
           });
 
           resolve();
         },
-        (error) => {
-          console.log('Error message: ' + error);
+        error => {
+          console.log("Error message: " + error);
           reject();
         }
       );
     });
   };
 
-  setCarName = (carToChange, carName) => {
+  updateCar = (idToChange, updatedName) => {
     const { cars } = this.state;
 
-    this.setState({
-      cars: cars.map((car) => ({
-        _id: car._id,
-        name: car._id === carToChange._id ? carName : car.name
-      })),
-    });
+    return axiosPut(`${apiURL}/${idToChange}`, {
+      name: updatedName
+    })
+      .then(() => {
+        this.setState({
+          cars: cars.map(car => ({
+            _id: car._id,
+            name: car._id === idToChange ? updatedName : car.name
+          }))
+        });
+      })
+      .catch(error => {
+        console.log("Error message: " + error);
+      });
   };
 
-  deleteCar = (carToChange) => {
+  deleteCar = idToDelete => {
     const { cars } = this.state;
 
-    return axiosDelete(apiURL, carToChange._id)
-      .then(
-        () => {
-          this.setState({
-            cars: cars.filter((car) => car._id !== carToChange._id)
-          });
-        },
-        (error) => {
-          console.log('Error message: ' + error);
-        }
-      )
+    return axiosDelete(apiURL, idToDelete).then(
+      () => {
+        this.setState({
+          cars: cars.filter(car => car._id !== idToDelete)
+        });
+      },
+      error => {
+        console.log("Error message: " + error);
+      }
+    );
   };
 
   render() {
-    const { cars } = this.state
+    const { cars } = this.state;
+
     return (
-        <Layout>
-          <img src={logo} className="App-logo" alt="logo" />
-          <Layout.Header>
-            <h2 style={{ color: "white", textAlign: "center", marginBottom: 30 }}>
-              Cars
-            </h2>
-          </Layout.Header>
-          <Layout.Content>
-            <Row type="flex" justify="center">
-                <FormCar addCar={this.addCar} cars={cars}/>
-            </Row>
-            <Row type="flex" justify="center">
-              <Col className="gutter-row" span={24}>
-                  {cars ? <Cars cars={cars} setCarName={this.setCarName} deleteCar={this.deleteCar}/> : <Spin /> }
-              </Col>
-            </Row>
-          </Layout.Content>
-        </Layout>
+      <Layout>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Layout.Header>
+          <h2 style={{ color: "white", textAlign: "center", marginBottom: 30 }}>
+            Cars
+          </h2>
+        </Layout.Header>
+        <Layout.Content>
+          <Row type="flex" justify="center">
+            <FormCar addCar={this.addCar} cars={cars} />
+          </Row>
+          <Row type="flex" justify="center">
+            <Col className="gutter-row" span={24}>
+              {cars ? (
+                <Cars
+                  cars={cars}
+                  deleteCar={this.deleteCar}
+                  updateCar={this.updateCar}
+                />
+              ) : (
+                <Spin />
+              )}
+            </Col>
+          </Row>
+        </Layout.Content>
+      </Layout>
     );
   }
 }
